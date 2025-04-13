d095e29029f46415456a3477a636a0fd8dc84c60
Касымалиев Айбек <aibek6060@gmail.com>
2025-04-13 16:04:50 +0300

    kasymaliev_aibek_homework_3

--- homework/Kasymaliev_Aibek/homework_3/task_4.py
+++ homework/Kasymaliev_Aibek/homework_3/task_4.py
@@ -1,7 +1,9 @@
 katet_1 = 5
 katet_2 = 6
 
-gip = (katet_1**2+katet_2**2)**(1/2)
-s = (1/2) * katet_1 * katet_2
+gip = (katet_1 ** 2 + katet_2 ** 2) ** (1 / 2)
+s = (1 / 2) * katet_1 * katet_2
+
 print(gip)
 print(s)
+
