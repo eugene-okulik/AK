048e9104b32d520a6623244c2601a39adec65d9e
Касымалиев Айбек <aibek6060@gmail.com>
2025-04-21 20:10:44 +0300

    kasymaliev_aibek_homework_7

---
+++ homework/Kasymaliev_Aibek/homework_7/task 3.py
@@ -0,0 +1,11 @@
+massage_1 = 'результат операции: 42'
+massage_2 = 'результат операции: 514'
+massage_3 = 'результат работы программы: 9'
+
+def massage_result(massage_for_function):
+    massage_index = int(massage_for_function.index(':')) + 1
+    print(massage_index + 10)
+
+massage_result(massage_1)
+massage_result(massage_2)
+massage_result(massage_3)
