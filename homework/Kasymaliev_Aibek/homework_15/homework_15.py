983ba592e434c17c61fdec4123eb4c8ea4f316f0
Касымалиев Айбек <aibek6060@gmail.com>
2025-08-30 21:27:33 +0300

    kasymaliev_aibek_homework_15

--- homework/Kasymaliev_Aibek/homework_15/homework_15.py
+++ homework/Kasymaliev_Aibek/homework_15/homework_15.py
@@ -14,12 +14,13 @@ add_student = 'insert into students (name, second_name) VALUES (%s, %s)'
 student_values = ('Maks', 'Pain')
 student = cursor.execute(add_student, student_values)
 student_id = cursor.lastrowid
-cursor.execute(f'select * from students where id = {student_id}')
+cursor.execute('select * from students where id = %s', (student_id,))
 new_student = cursor.fetchone()
 print(
     f"Student: id = {new_student['id']}, name = {new_student['name']}, second_name = {new_student['second_name']}, "
     f"group_id = {new_student['group_id']}"
 )
+
 
 books_values = [
     ('Harry Potter', student_id),
@@ -27,29 +28,24 @@ books_values = [
     ('1984', student_id),
     ('Three Comrades', student_id)
 ]
-for book in books_values:
-    cursor.execute('insert into books (title, taken_by_student_id) values (%s, %s)', book)
-    book_id = cursor.lastrowid
-    cursor.execute(f'select * from books where id = {book_id}')
-    books = cursor.fetchone()
-    print(
-        f" Book_for_ student: id = {books['id']}, title = {books['title']},"
-        f"taken_by_student_id = {books['taken_by_student_id']}"
-    )
+
+add_books = 'INSERT INTO books (title, taken_by_student_id) VALUES (%s, %s)'
+cursor.executemany(add_books, books_values)
+
 
 add_group = 'INSERT INTO  `groups` (title, start_date, end_date) VALUES (%s, %s, %s)'
 student_values = ('Part_of_cinema', '01.01.2025', '01.01.2999')
 group = cursor.execute(add_group, student_values)
 group_id = cursor.lastrowid
-cursor.execute(f"SELECT * FROM `groups` WHERE id = {group_id}")
+cursor.execute("SELECT * FROM `groups` WHERE id = %s", (group_id,))
 new_group = cursor.fetchone()
 print(
     f"Group: id = {new_group['id']}, title = {new_group['title']},start_date = {new_group['start_date']},"
     f" end_date= {new_group['end_date']}"
 )
 
-cursor.execute(f'update students s set group_id = {group_id} where id = {student_id}')
-cursor.execute(f"select * from students where id = {student_id}")
+cursor.execute('update students s set group_id = %s where id = %s', (group_id, student_id,))
+cursor.execute("select * from students where id = %s", (student_id,))
 update_student_id = cursor.fetchone()
 print(
     f"Student_group: id = {update_student_id['id']}, name = {update_student_id['name']},"
@@ -57,13 +53,13 @@ print(
 )
 
 subject_values = [
-    ('Defense Aganist the Dark Arts',),
-    ('Charms',),
-    ('Potions',)
+    'Defense Aganist the Dark Arts',
+    'Charms',
+    'Potions'
 ]
 inserted_subjects = []
 for subject in subject_values:
-    cursor.execute('insert into subjects (title) values (%s)', subject)
+    cursor.execute('insert into subjects (title) values (%s)', (subject,))
     subject_id = cursor.lastrowid
     inserted_subjects.append({
         'id': subject_id,
@@ -72,18 +68,25 @@ for subject in subject_values:
     print(f"Subjects: id = {subject_id}, title = {subject}")
 
 
+inserted_lesson = []
+
+
 def lesson_function(subject, lessons_values):
-    cursor.execute("select id from subjects where title = %s", (subject,))
-    subject_id = cursor.fetchone()['id']
+    for subject_in_dictionary in inserted_subjects:
+        if subject == subject_in_dictionary['title']:
+            subject_id = subject_in_dictionary['id']
     for lesson in lessons_values:
         add_lesson = 'INSERT into lessons (title, subject_id) VALUES (%s, %s)'
         cursor.execute(add_lesson, (lesson, subject_id))
         lesson_id = cursor.lastrowid
-        cursor.execute("SELECT * FROM `lessons` WHERE id = %s", (lesson_id,))
-        new_lesson = cursor.fetchone()
+        inserted_lesson.append({
+            'id': lesson_id,
+            'title': lesson,
+            'subject_id': subject_id
+        })
         print(
-            f" Lessons: id = {new_lesson['id']}, title = {new_lesson['title']},"
-            f"subject_id = {new_lesson['subject_id']}"
+            f" Lessons: id = {lesson_id}, title = {lesson},"
+            f"subject_id = {subject_id}"
         )
 
 
@@ -93,13 +96,10 @@ lesson_function('Potions', ('Techniques', 'Common Charms',))
 
 
 def marks_function(mark, lesson, student):
-    cursor = db.cursor(dictionary=True, buffered=True)
-    cursor.execute("select id from lessons where title = %s", (lesson,))
-    lesson_row = cursor.fetchone()
-    lesson_id = lesson_row['id']
-    cursor.execute('select id from students where name = %s', (student,))
-    student_row = cursor.fetchone()
-    student_id = student_row['id']
+    for lesson_in_dictionary in inserted_lesson:
+        if lesson_in_dictionary['title'] == lesson:
+            lesson_id = lesson_in_dictionary['id']
+
     add_mark = "INSERT INTO marks (value, lesson_id, student_id) VALUES (%s, %s, %s)"
     cursor.execute(add_mark, (mark, lesson_id, student_id))
     mark_id = cursor.lastrowid
