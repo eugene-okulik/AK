773d55c5f683df16721e09ddf533c43c4abdafe5
Касымалиев Айбек <aibek6060@gmail.com>
2025-09-04 01:00:10 +0300

    kasymaliev_aibek_homework_16

---
+++ homework/Kasymaliev_Aibek/homework_16/task_1.py
@@ -0,0 +1,40 @@
+import mysql.connector as mysql
+import os
+import dotenv
+import csv
+
+
+dotenv.load_dotenv()
+db = mysql.connect(
+    user=os.getenv('DB_USER'),
+    passwd=os.getenv('DB_PASSW'),
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT'),
+    database=os.getenv('DB_DATABASE')
+)
+
+
+with open('C:\\Users\\aibek\\Desktop\\test_pro\\test\\AK\\homework\\eugene_okulik\\Lesson_16\\hw_data\\data.csv',
+          newline='') as csv_file:
+    file_data = csv.DictReader(csv_file)
+    data = []
+    for row in file_data:
+        data.append(row)
+        cursor = db.cursor(dictionary=True)
+        info_from_database_about_student = """
+        SELECT s.name as name, s.second_name as second_name, g.title as group_title, b.title as book_title,
+        s2.title as subject_title, l.title as lesson_title, m.value as mark_value
+        FROM students s
+        JOIN `groups` g ON g.id = s.group_id
+        JOIN books b ON b.taken_by_student_id = s.id
+        JOIN marks m ON m.student_id = s.id
+        JOIN lessons l ON m.lesson_id = l.id
+        JOIN subjects s2 ON s2.id = l.subject_id
+        WHERE s.name = %s and s.second_name = %s and g.title = %s and b.title = %s and s2.title = %s and l.title = %s
+        and m.value = %s
+        """
+        cursor.execute(info_from_database_about_student, (row['name'], row['second_name'], row['group_title'],
+                                                          row['book_title'], row['subject_title'], row['lesson_title'],
+                                                          row['mark_value']))
+        for row_data in cursor:
+            print(row_data)
